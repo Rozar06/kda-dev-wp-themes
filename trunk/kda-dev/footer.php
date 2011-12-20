f50b01fc58e149e469be29102d01bb84c7ceaa97
knguyen1015@gmail.com <knguyen1015@gmail.com@2f87c618-3bdc-cd14-e06d-8f856074d130>
2011-12-20 21:06:01 +0000

    adding kda-dev theme

---
+++ trunk/kda-dev/footer.php
@@ -0,0 +1,6 @@
+</div> <!-- end wrapper -->
+<div id="footer_bg">
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ trunk/kda-dev/page-services.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Template Name: page - Services
+*/
+?>
+
+<?php get_header(); ?>
+	
+	<div id="serviceSlider" class="grid_12">
+		
+	<div class="slider-wrap">
+			
+	<div id="slider1" class="csw">
+		
+		<div class="panelContainer">
+			
+			<div class="panel" title="Design">
+				<div class="wrapper">
+					<h1>Design</h1>
+					<p class="catchPhrase">You dream it, we design it.</p>
+					<p class="floatLeft">
+					<img src="design.jpg" class="floatRight" />
+					Designing an impressive professional website requires a blend of artistic style and logical structure. KDA Development designs websites to be attractive, functional, and
+					usable. Portraying the image that you want for your brand on the World Wide Web is our primary goal. Let us turn your dream into a reality, contact us today for a free consultation.</p>
+				</div>
+			</div>
+			<div class="panel" title="Development">
+				<div class="wrapper">
+					<h1>Development</h1>
+					<p class="catchPhrase">Development is where the rubber meets the road.</p>
+					<p class="floatLeft">
+					<img src="development.jpg" class="floatRight" />
+					Designing an impressive professional website requires a blend of artistic style and logical structure. KDA Development designs websites to be attractive, functional, and
+					usable. Portraying the image that you want for your brand on the World Wide Web is our primary goal. Let us turn your dream into a reality, contact us today for a free consultation.</p>
+				</div>
+			</div>		
+			<div class="panel" title="Content Management">
+				<div class="wrapper">
+					<h1>Content Management</h1>
+					<p class="catchPhrase">Take control of your content.</p>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus porta tortor sed metus. Nam pretium. Sed tempor. Integer ullamcorper, odio quis porttitor sagittis, nisl erat tincidunt massa, eu eleifend eros nibh sollicitudin est. Nulla dignissim. Mauris sollicitudin, arcu id sagittis placerat, tellus mauris egestas felis, eget interdum mi nibh vel lorem. Aliquam egestas hendrerit massa. Suspendisse sed nunc et lacus feugiat hendrerit. Nam cursus euismod augue. Aenean vehicula nisl eu quam luctus adipiscing. Nunc consequat justo pretium orci. Mauris hendrerit fermentum massa. Aenean consectetuer est ut arcu. Aliquam nisl massa, blandit at, accumsan sed, porta vel, metus. Duis fringilla quam ut eros.</p>
+					<p>Sed eu ligula eget eros vulputate tincidunt. Etiam sapien urna, auctor a, viverra sit amet, convallis a, enim. Nullam ut nulla. Nam laoreet massa aliquet tortor. Mauris in quam ut dui bibendum malesuada. Nulla vel erat. Pellentesque metus risus, aliquet eget, eleifend in, ultrices vitae, nisi. Vivamus non nulla. Praesent ac lacus. Donec augue turpis, convallis sed, lacinia et, vestibulum nec, lacus. Suspendisse feugiat semper nunc. Donec nisl elit, varius sed, sodales volutpat, commodo in, elit. Proin ornare hendrerit lectus. Sed non dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis suscipit. Mauris egestas tincidunt lectus. Phasellus sed quam et velit laoreet pretium. Nunc metus.</p>
+					<p><a href="#2" class="cross-link" title="Go to Panel 2">&#171; Previous</a> | <a href="#4" class="cross-link" title="Go to Panel 4">Next &#187;</a></p>
+				</div>
+			</div>
+			<div class="panel" title="Internet Marketing">
+				<div class="wrapper">
+					<h1>Internet Marketing</h1>
+					<p class="catchPhrase">Harness the power of the Internet to market your business.</p>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus porta tortor sed metus. Nam pretium. Sed tempor. Integer ullamcorper, odio quis porttitor sagittis, nisl erat tincidunt massa, eu eleifend eros nibh sollicitudin est. Nulla dignissim. Mauris sollicitudin, arcu id sagittis placerat, tellus mauris egestas felis, eget interdum mi nibh vel lorem. Aliquam egestas hendrerit massa. Suspendisse sed nunc et lacus feugiat hendrerit. Nam cursus euismod augue. Aenean vehicula nisl eu quam luctus adipiscing. Nunc consequat justo pretium orci. Mauris hendrerit fermentum massa. Aenean consectetuer est ut arcu. Aliquam nisl massa, blandit at, accumsan sed, porta vel, metus. Duis fringilla quam ut eros.</p>
+					<p>Sed eu ligula eget eros vulputate tincidunt. Etiam sapien urna, auctor a, viverra sit amet, convallis a, enim. Nullam ut nulla. Nam laoreet massa aliquet tortor. Mauris in quam ut dui bibendum malesuada. Nulla vel erat. Pellentesque metus risus, aliquet eget, eleifend in, ultrices vitae, nisi. Vivamus non nulla. Praesent ac lacus. Donec augue turpis, convallis sed, lacinia et, vestibulum nec, lacus. Suspendisse feugiat semper nunc. Donec nisl elit, varius sed, sodales volutpat, commodo in, elit. Proin ornare hendrerit lectus. Sed non dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis suscipit. Mauris egestas tincidunt lectus. Phasellus sed quam et velit laoreet pretium. Nunc metus.</p>
+					<p><a href="#3" class="cross-link" title="Go to Panel 3">&#171; Previous</a> | <a href="#5" class="cross-link" title="Go to Panel 5">Next &#187;</a></p>
+				</div>
+			</div>
+			<div class="panel" title="Support">
+				<div class="wrapper">
+					<h1>Support</h1>
+					<p class="catchPhrase">Providing ongoing support for your website.</p>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus porta tortor sed metus. Nam pretium. Sed tempor. Integer ullamcorper, odio quis porttitor sagittis, nisl erat tincidunt massa, eu eleifend eros nibh sollicitudin est. Nulla dignissim. Mauris sollicitudin, arcu id sagittis placerat, tellus mauris egestas felis, eget interdum mi nibh vel lorem. Aliquam egestas hendrerit massa. Suspendisse sed nunc et lacus feugiat hendrerit. Nam cursus euismod augue. Aenean vehicula nisl eu quam luctus adipiscing. Nunc consequat justo pretium orci. Mauris hendrerit fermentum massa. Aenean consectetuer est ut arcu. Aliquam nisl massa, blandit at, accumsan sed, porta vel, metus. Duis fringilla quam ut eros.</p>
+					<p>Sed eu ligula eget eros vulputate tincidunt. Etiam sapien urna, auctor a, viverra sit amet, convallis a, enim. Nullam ut nulla. Nam laoreet massa aliquet tortor. Mauris in quam ut dui bibendum malesuada. Nulla vel erat. Pellentesque metus risus, aliquet eget, eleifend in, ultrices vitae, nisi. Vivamus non nulla. Praesent ac lacus. Donec augue turpis, convallis sed, lacinia et, vestibulum nec, lacus. Suspendisse feugiat semper nunc. Donec nisl elit, varius sed, sodales volutpat, commodo in, elit. Proin ornare hendrerit lectus. Sed non dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis suscipit. Mauris egestas tincidunt lectus. Phasellus sed quam et velit laoreet pretium. Nunc metus.</p>
+					<p><a href="#4" class="cross-link" title="Go to Panel 4">&#171; Previous</a> | <a href="#1" class="cross-link" title="Go to Panel 1">Next &#187;</a></p>
+				</div>
+			</div>
+			
+		</div><!-- .panelContainer -->
+	</div><!-- #slider1 -->
+</div><!-- .slider-wrap -->
+
+	
+		<!--<div id="contentSlide"></div><!-- close contentSlide -->
+		
+	</div><!-- close serviceSlider -->
+
+<?php get_footer(); ?>
